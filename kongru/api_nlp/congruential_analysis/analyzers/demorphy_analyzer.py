202f953219e0970bfc67f2e12f0a024c5ca213ae
Christopher Michael Chandler <christopher.chandler@outlook.de>
2023-08-17 08:38:42 +0200

    Re-add analyzers

--- kongru/api_nlp/congruential_analysis/analyzers/demorphy_analyzer.py
+++ kongru/api_nlp/congruential_analysis/analyzers/demorphy_analyzer.py
@@ -1,3 +1,41 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7308fd745526527f332cd65cbc7bb3b29b76e2660c88145bf6fb9a4e3d193015
-size 843
+# Standard
+# None
+
+# Pip
+# None
+
+# Custom
+
+# general
+from kongru.api_general.data_parsers.demorphy_parser import DemorphyParser
+
+# nlp
+from kongru.api_nlp.congruential_analysis.analyzers.suffix_analyzer import (
+    SuffixAnalyzer,
+)
+from kongru.api_nlp.congruential_analysis.analyzers.token_analyzer import TokenAnalyzer
+
+
+class DemorphyAnalyzer(DemorphyParser, SuffixAnalyzer, TokenAnalyzer):
+    def __init__(self, word):
+        self.word = word
+
+    def analyze_by_dafsa(self):
+        pass
+
+    def analyze_by_ending(self):
+        pass
+
+    def analyze(self):
+        pass
+
+    def is_know(self):
+        pass
+
+    def iter_lexicon_formatted(self, prefix=""):
+        pass
+
+
+if __name__ == "__main__":
+    res = DemorphyAnalyzer(word="gegangen")
+    print(res.guess_word_by_suffix())
